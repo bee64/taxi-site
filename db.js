255fab764f42850d956cca90388cb9814eb4bdd1
Brad-- <brad.smith.1324@gmail.com>
2016-04-26 20:32:02 -0600

    Added port to config file

--- db.js
+++ db.js
@@ -5,12 +5,13 @@ var bodyParser = require('body-parser');
 var request = require('request');
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '/public')));
-
-var API_KEY   = require('./config.json').API_KEY;
-var INTERCEPT = require('./config.json').INTERCEPT;
-var XONE      = require('./config.json').XONE;
-var XTWO      = require('./config.json').XTWO;
-var XTHREE    = require('./config.json').XTHREE;
+var json = require('./config.json');
+var API_KEY   = json.API_KEY;
+var INTERCEPT = json.INTERCEPT;
+var XONE      = json.XONE;
+var XTWO      = json.XTWO;
+var XTHREE    = json.XTHREE;
+var PORT      = json.PORT;
 
 var urlTop = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input="
 var urlBtm = "&location=40.7128,-74.0059&radius=60500&key=" + API_KEY;
@@ -53,6 +54,6 @@ app.post('/fare', function(req, res) {
 		res.status(500).send('Invalid data!');
 });
 
-app.listen(3000, function(){
-	console.log('Server listening on port 3000');
+app.listen(PORT, function(){
+	console.log('Server listening on port ' + PORT);
 });
